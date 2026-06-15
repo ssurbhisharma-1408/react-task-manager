1e110b4e729e36ec109960606e5b8bb4c0b637d2
ssurbhisharma-1408 <susharma@trueigtech.com>
2026-06-15 14:48:09 +0530

    feat:added search bar using debouncing

--- react-task-manager/src/App.jsx
+++ react-task-manager/src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import TaskModal from "./components/TaskModal";
 import TaskColumn from "./components/TaskColumn";
 import "./App.css";
@@ -17,6 +17,25 @@ export default function App() {
   };
    const [showModal, setShowModal] = useState(false);
    const [editingTask, setEditingTask] = useState(null);
+   const [count,setCounter] = useState(0);
+   const [searchInput,setSearchInput]=useState("");
+   const [searchQuery,setSearchQuery] = useState("");
+
+    const increment =()=>{
+      setCounter(prev => prev+2);
+      setTimeout(()=>{
+         setCounter(prev=> prev+2);
+      },3000)
+    }
+     useEffect (()=>{
+      const timer = setTimeout(()=>{
+        setSearchQuery(searchInput);
+      },2000);
+
+      return ()=>clearTimeout(timer);
+      
+     },[searchInput]);
+
 
     const handleSave = ({ title, description }) => {
     if (editingTask) {
@@ -62,6 +81,52 @@ export default function App() {
           <h1>Task Manager</h1>
           <span className="total-badge">{totalActive} Active Tasks</span>
         </div>
+
+          <div className="search-wrapper">
+            <input type="text" 
+            className="search-input"
+            value={searchInput}
+            placeholder="Search by title or description"
+            onChange={e=>setSearchInput(e.target.value)}/>
+            {searchInput && (
+            <button className="search-clear" onClick={() => { setSearchInput(""); setSearchQuery(""); }}>
+              X
+            </button>
+          )}
+          {searchQuery.trim() && (
+  <div className="search-dropdown">
+    {(() => {
+      const results = tasks.filter(t => {
+        const keyword = searchQuery.toLowerCase();
+        return (
+          (t.title).toLowerCase().includes(keyword) ||
+          (t.description).toLowerCase().includes(keyword)
+        );
+      });
+
+      return results.length === 0 ? (
+        <p className="search-empty">No tasks found for "{searchQuery}"</p>
+      ) : (
+        results.map(t => (
+          <div key={t.id} className="search-result-item">
+            <div className="search-result-top">
+              <span>{t.title}</span>
+              <span>
+                {t.status}
+              </span>
+            </div>
+            {t.description && (
+              <p>{t.description}</p>
+            )}
+          </div>
+        ))
+      );
+    })()}
+  </div>
+)}
+          </div>
+
+
         <button className="btn-add-task" onClick={openAddModal}>
           + New Task
         </button>
@@ -71,13 +136,19 @@ export default function App() {
           <TaskColumn
             key={col}
             status={col}
-            tasks={tasks.filter(t => t.status === col)}
+            tasks={tasks.filter(t=>t.status===col)}
             onDeletePermanently={deletePermanently}
             onEdit={openEditModal}
             onDrop={handleDrop} 
           />
         ))}
       </div>
+    
+          <div className = "counter-btns">
+          <button className="counter-btn" onClick={increment}> Counter {count}</button>
+          <button className= "counter-btn" onClick={()=>setCounter(0)}> Reset</button>
+          </div>
+       
 
       {showModal && (
         <TaskModal
@@ -86,6 +157,8 @@ export default function App() {
           onClose={() => { setShowModal(false); setEditingTask(null); }}
         />
       )}
+       
+       
     </div>
   );
 }
